c9430d3480f467c2f80ec55bb421acf7ccf1abd5
xiaojia <2434220573@qq.com>
2016-04-21 00:20:15 +0800

    4.21

--- js/copyrightshow.js
+++ js/copyrightshow.js
@@ -1,10 +1,15 @@
 $(function(){
 	
     $('#dowebok').fullpage({
-    	     sectionsColor: ['#ff33ff', '#4BBFC3', '#7BAABE','#ffff00'],
+    	     sectionsColor: ['#ff33ff', '#efefef', '#7BAABE','#CCCCCC'],
     	     scrollingSpeed: 600,
+//             verticalCentered: false,
     	     afterLoad: function(anchorLink, index){
-    	     	console.log(index)
+    	     	console.log(index);
+                 if(index == 2){
+                    $('.copy-do-animation').animate({'marginLeft':'-505px'},'slow');
+                    $('.copy-do-copy').animate({'marginLeft':'195px'},'slow');
+                 }
     	     }
     });
 });
