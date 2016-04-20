c88acef6237c42f61a64a4007845f85cc68ab256
minet <2944162490@qq.com>
2016-04-20 23:26:20 +0800

    add#[one]

--- js/copyrightshow.js
+++ js/copyrightshow.js
@@ -1,10 +1,29 @@
 $(function(){
-	
     $('#dowebok').fullpage({
-    	     sectionsColor: ['#ff33ff', '#4BBFC3', '#7BAABE','#ffff00'],
-    	     scrollingSpeed: 600,
+         anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage','fivePage', 'lastPage'],
+         menu: '#copy-menu-item',
+    	     sectionsColor: ['#ffffff', '#4BBFC3', '#7BAABE','#ffff00'],
+    	     scrollingSpeed: 500,
     	     afterLoad: function(anchorLink, index){
-    	     	console.log(index)
+    	     	$("#copy-menu-item li a").removeClass("copy-active");
+    	     	$("#copy-menu-item li").eq(index-1).children("a").addClass("copy-active");
+    	     	if(index == 1){
+    	     		$(".copy-video").animate({height:"150px"},300,function(){
+    	     		      
+    	     	    });
+    	     	}
+    	     },
+    	     onLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){
+    	     	   //alert("1111")
+    	     	   $(".copy-video").animate({height:"0px"},200,function(){
+    	     		
+    	     	    });
     	     }
+    	     
+    });
+    
+    $("#copy-menu-item li").click(function(){
+    	     $(this).children("a").addClass("copy-active");
+    	     $(this).siblings().children("a").removeClass("copy-active");
     });
 });
