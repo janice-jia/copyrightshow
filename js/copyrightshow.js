8abf77f5916b9fd5b9c0a2c198463224750ee2e8
minet <2944162490@qq.com>
2016-04-20 20:52:47 +0800

    first

---
+++ js/copyrightshow.js
@@ -0,0 +1,10 @@
+$(function(){
+	
+    $('#dowebok').fullpage({
+    	     sectionsColor: ['#ff33ff', '#4BBFC3', '#7BAABE','#ffff00'],
+    	     scrollingSpeed: 600,
+    	     afterLoad: function(anchorLink, index){
+    	     	alert(index)
+    	     }
+    });
+});
